4746f126668591b217bf38f4cfa56063e5d4c4b4
Andres Sepulveda <holberton@NNRX2000>
2021-06-25 19:18:20 -0500

    we finished 4th and We are in 5th.

--- models/4base_model.py
+++ models/4base_model.py
@@ -13,12 +13,20 @@ class BaseModel:
         Inicialize first values for
         atributes: id, created_at, updated_at
         """
-        if kwargs:
-            
+        if not kwargs: # Here goes an exeption
+            # What if kwargs is empty?
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
-        self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        else:
+            self.__dict__ = kwargs # unittest, What if not atribute __class__ in dictionary
+            if "__class__" in self.__dict__.keys():
+                self.__dict__.pop("__class__")
+                formato = "%Y-%m-%dT%H:%M:%S.%f"
+            self.updated_at = datetime.strptime(self.updated_at, formato)
+            self.created_at = datetime.strptime(self.created_at, formato)
+
 
     def __str__(self):
         """
@@ -45,14 +53,25 @@ class BaseModel:
 
 
 
+
 my_model = BaseModel()
 my_model.name = "Holberton"
 my_model.my_number = 89
+print(my_model.id)
 print(my_model)
-my_model.save()
-print(my_model)
+print(type(my_model.created_at))
+print("--")
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Storage Data of instances
+"""
+
+class FileStorage:
+
+
+    __file_path = "file.json"
+    __objects = {}
+
+
+    def all(self):
+        """Returns a diccionary of ALL objects """
+        return __objects
+
+    def new(self, obj):
+        """ """
+
+
+
+    def save(self):
+
+    def reload(self):
